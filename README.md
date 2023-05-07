aefc80c64f65d7927cc46bbc4d51c010a213083f
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-07 15:44:55 +0200

    Assign css attributes

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Create a web app with React that allows for users to generate and download memes
 
 It should allow the user to:
 
-- [ ] Enter text for the top and bottom of the meme
+- [x] Enter text for the top and bottom of the meme
 
-  - [ ] The top text box needs to have a label element associated with it containing the text `Top text`
+  - [x] The top text box needs to have a label element associated with it containing the text `Top text`
 
-  - [ ] The bottom text box needs to have a label element associated with it with the text `Bottom text`
+  - [x] The bottom text box needs to have a label element associated with it with the text `Bottom text`
 
-  - [ ] Both text boxes should be empty when the page first loads
+  - [x] Both text boxes should be empty when the page first loads
 
 - [ ] Preview the generated meme
 
@@ -21,7 +21,7 @@ It should allow the user to:
 
 - [ ] Change the meme template (the background image)
 
-  - [ ] The meme template selector element needs to have a label element associated with it containing the text `Meme template`
+  - [x] The meme template selector element needs to have a label element associated with it containing the text `Meme template`
   - [ ] If the user follows the steps below, the `doge` meme template needs to be selected:
     1. Click on the label of the meme template selector
     2. Clear any existing value (eg. with a text box)
--- src/App.js
+++ src/App.js
@@ -1,77 +1,35 @@
 import './App.css';
-import nodeEmoji from 'node-emoji';
 import { useState } from 'react';
 
-export default function App() {
-  // 1. Create a state variable and setter, with
-  // a default value of a random emoji
-  const [emoji, setEmoji] = useState(nodeEmoji.random().emoji);
-  const [emojiName, setEmojiName] = useState('');
-  const hasError = emojiName !== '' && !nodeEmoji.hasEmoji(emojiName);
+const baseURL = 'https://api.memegen.link/images/';
 
-  // const [hasError, setHasError] = useState(false);
+export default function App() {
+  const [memeName, setMemeName] = useState('');
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
 
   return (
     <div className="App">
       <div>
-        <p>Meme Generator</p>
-        {emoji}
-        <br />
-        <span>Meme template</span>
-        <br />
-        <input
-          value={emojiName}
-          onChange={(event) => {
-            setEmojiName(event.currentTarget.value);
-            if (nodeEmoji.hasEmoji(event.currentTarget.value)) {
-              // setHasError(false);
-              setEmoji(nodeEmoji.find(event.currentTarget.value).emoji);
-              // } else {
-              //   setHasError(true);
-            }
-          }}
-        />{' '}
-        <br />
-        <span>Top text</span>
-        <br />
-        <input
-          value={emojiName}
-          onChange={(event) => {
-            setEmojiName(event.currentTarget.value);
-            if (nodeEmoji.hasEmoji(event.currentTarget.value)) {
-              // setHasError(false);
-              setEmoji(nodeEmoji.find(event.currentTarget.value).emoji);
-              // } else {
-              //   setHasError(true);
-            }
-          }}
-        />{' '}
-        <br />
-        <span>Bottom text</span>
-        <br />
-        <input
-          value={emojiName}
-          onChange={(event) => {
-            setEmojiName(event.currentTarget.value);
-            if (nodeEmoji.hasEmoji(event.currentTarget.value)) {
-              // setHasError(false);
-              setEmoji(nodeEmoji.find(event.currentTarget.value).emoji);
-              // } else {
-              //   setHasError(true);
-            }
-          }}
-        />{' '}
-        <br />
-        <button
-          onClick={() => {
-            // 3. Generate and set a new value for the emoji
-            const newEmoji = nodeEmoji.random().emoji;
-            setEmoji(newEmoji);
-          }}
-        >
-          Download
-        </button>
-        {hasError && 'x'}
+        <header>Meme Generator</header>
+
+        <div>
+          <span>Meme template</span>
+          <input />
+          <span>Top text</span>
+          <input />
+          <span>Bottom text</span>
+          <input />
+        </div>
+
+        <img
+          src="https://reactjs.org/logo-og.png"
+          alt="react logo"
+          style={{ width: '300px' }}
+        />
+        <div>
+          <button onClick={() => {}}>Download</button>
+        </div>
       </div>
     </div>
   );
